c91d25a734dab0d568448922f260e210f68ccaf0
Khant Si Thu Win <khantsithuwin00@gamil.com>
2025-09-30 20:14:43 +0700

    add validation to the sign up page

--- lib/features/auth/signup/pages/signup_page.dart
+++ lib/features/auth/signup/pages/signup_page.dart
@@ -11,6 +11,11 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final GlobalKey<FormState> _signupFormKey = GlobalKey<FormState>();
+  String? _name;
+  String? _email;
+  String? _password;
+
   @override
   Widget build(BuildContext context) {
     ColorScheme colorScheme = Theme.of(context).colorScheme;
@@ -21,59 +26,109 @@ class _SignupPageState extends State<SignupPage> {
       appBar: AppBar(title: Text("Sign Up")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              style: textTheme.bodyLarge?.copyWith(
-                color: colorScheme.onSurfaceVariant,
-              ),
-              decoration: InputDecoration(
-                fillColor: colorScheme.surfaceContainerHighest,
-                filled: true,
-                hintText: "Email",
-                border: InputBorder.none,
-              ),
-            ),
-            SizedBox(height: 8.0),
-            TextFormField(
-              style: textTheme.bodyLarge?.copyWith(
-                color: colorScheme.onSurfaceVariant,
-              ),
-              decoration: InputDecoration(
-                fillColor: colorScheme.surfaceContainerHighest,
-                filled: true,
-                hintText: "Password",
-                border: InputBorder.none,
-              ),
-            ),
-            SizedBox(height: 16.0),
-            BrandButtonWidget(text: "Login", onPressed: () {}),
-            SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already have an account?",
-                  style: textTheme.bodyMedium?.copyWith(
-                    color: colorScheme.onSurface,
-                  ),
+        child: Form(
+          key: _signupFormKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                style: textTheme.bodyLarge?.copyWith(
+                  color: colorScheme.onSurfaceVariant,
                 ),
-                SizedBox(width: 8.0),
-                InkWell(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text(
-                    "Login",
+                decoration: InputDecoration(
+                  fillColor: colorScheme.surfaceContainerHighest,
+                  filled: true,
+                  hintText: "Name",
+                  border: InputBorder.none,
+                ),
+                validator: (String? name) {
+                  if (name != null && name.length < 3) {
+                    return "Please enter name above 3 characters";
+                  }
+                  return null;
+                },
+                onSaved: (String? name) {
+                  _name = name;
+                },
+              ),
+              SizedBox(height: 8.0),
+              TextFormField(
+                style: textTheme.bodyLarge?.copyWith(
+                  color: colorScheme.onSurfaceVariant,
+                ),
+                decoration: InputDecoration(
+                  fillColor: colorScheme.surfaceContainerHighest,
+                  filled: true,
+                  hintText: "Email",
+                  border: InputBorder.none,
+                ),
+                validator: (String? email) {
+                  if (email == null || email.isEmpty || !email.contains('@')) {
+                    return "Please enter Email correctly";
+                  }
+                  return null;
+                },
+                onSaved: (String? email) {
+                  _email = email;
+                },
+              ),
+              SizedBox(height: 8.0),
+              TextFormField(
+                obscureText: true,
+                style: textTheme.bodyLarge?.copyWith(
+                  color: colorScheme.onSurfaceVariant,
+                ),
+                decoration: InputDecoration(
+                  fillColor: colorScheme.surfaceContainerHighest,
+                  filled: true,
+                  hintText: "Password",
+                  border: InputBorder.none,
+                ),
+                validator: (String? password) {
+                  if (password != null && password.length < 6) {
+                    return "Password must have at least 6";
+                  }
+                  return null;
+                },
+                onSaved: (String? password) {
+                  _password = password;
+                },
+              ),
+              SizedBox(height: 16.0),
+              BrandButtonWidget(
+                text: "Login",
+                onPressed: () {
+                  FormState? formState = _signupFormKey.currentState;
+                  formState?.save();
+                  if (formState?.validate() == true) {}
+                },
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
                     style: textTheme.bodyMedium?.copyWith(
-                      color: colorBrand.brandDefault,
+                      color: colorScheme.onSurface,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(width: 8.0),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Login",
+                      style: textTheme.bodyMedium?.copyWith(
+                        color: colorBrand.brandDefault,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
